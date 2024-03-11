7b6f29d082a07316bdc73c0f111f7ec0072b3ce8
LoGinShin <95666408+loginshin@users.noreply.github.com>
2024-03-11 15:48:09 +0900

    단방향 세팅

--- jpaPostgre/src/main/java/Main.java
+++ jpaPostgre/src/main/java/Main.java
@@ -1,10 +1,13 @@
-import java.lang.reflect.Member;
+
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import domain.Member;
+import domain.Team;
 
 public class Main {
 
@@ -20,7 +23,24 @@ public class Main {
 		
 		try {
 			
+			Team team = new Team();
+			team.setName("TeamA");
+			entityManager.persist(team);
+			
+			Member member = new Member();
+			member.setUsername("member1");
+			member.setTeam(team); // 팀 지정
+			entityManager.persist(member); //영속성 컨텍스트에 들어가 있다.
 
+			// db에서 값을 가져오고싶다면
+			entityManager.flush();
+			entityManager.clear();
+			
+			
+			// fine로 조회하기
+			Member findMember = entityManager.find(Member.class, member.getId()); // find하면 1차 캐쉬에서 가지고 온것이다.
+			Team findTeam = findMember.getTeam();
+			System.out.println("find name = " + findTeam.getName());
 
 			
 			entityTransaction.commit();
--- jpaPostgre/src/main/java/domain/Member.java
+++ jpaPostgre/src/main/java/domain/Member.java
@@ -4,50 +4,53 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Member {
-	
+
 	@Id @GeneratedValue
 	@Column(name = "MEMBER_ID")
 	private Long id;
-	private String name;
-	private String city;
-	private  String street;
-	private  String zpiconde;
 	
-	// ================================
+	@Column(name = "USERNAME")
+	private String username;
+	
+	@ManyToOne
+	@JoinColumn(name = "TEAM_ID")
+	private Team team;
+
+	// ===================================
 	
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
+
+
+	public String getUsername() {
+		return username;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
-	public String getCity() {
-		return city;
+
+	public Team getTeam() {
+		return team;
 	}
-	public void setCity(String city) {
-		this.city = city;
+
+	public void setTeam(Team team) {
+		this.team = team;
 	}
-	public String getStreet() {
-		return street;
-	}
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	public String getZpiconde() {
-		return zpiconde;
-	}
-	public void setZpiconde(String zpiconde) {
-		this.zpiconde = zpiconde;
-	}
+	
+	
+	
+	
 	
 	
 }
