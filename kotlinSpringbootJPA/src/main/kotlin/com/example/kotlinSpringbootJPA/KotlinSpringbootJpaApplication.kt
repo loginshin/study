2f8e18281e14a0a48e6c3f01aecff03754ee1b57
LoGinShin <95666408+loginshin@users.noreply.github.com>
2024-03-18 20:41:41 +0900

    reset my study kopringboot JPA

---
+++ kotlinSpringbootJPA/src/main/kotlin/com/example/kotlinSpringbootJPA/KotlinSpringbootJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinSpringbootJPA
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringbootJpaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringbootJpaApplication>(*args)
+}
